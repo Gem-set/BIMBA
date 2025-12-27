9ee82cc5ef4c64cdba90ca087f6872cc79592c72
Gem-set <bogdaney.2207@gmail.com>
2025-12-27 16:55:02 +0500

    Add files via upload

--- сайт/script.js
+++ сайт/script.js
@@ -6,7 +6,7 @@ const translations = {
     home: "Главная",
     about: "О сайте",
     learn: "Темы",
-    contact: "Контакты",
+    test: "Тесты",
     stbt: "Начать",
     title: "EduWeb",
     topics: "Темы",
@@ -19,7 +19,7 @@ const translations = {
     home: "Home",
     about: "About",
     learn: "Topics",
-    contact: "Contacts",
+    test: "Tests",
     stbt: "Start",
     title: "EduWeb",
     topics: "Topics",
@@ -71,21 +71,88 @@ changeLanguage(savedLang);
 const startBtn = document.getElementById("strbtn");
 const circle = document.getElementById("transition");
 
-startBtn.addEventListener("click", () => {
-  const rect = startBtn.getBoundingClientRect();
+if (startBtn && circle) {
+  startBtn.addEventListener("click", () => {
+    const rect = startBtn.getBoundingClientRect();
 
- 
-  circle.style.left = rect.left + rect.width / 2 + "px";
-  circle.style.top = rect.top + rect.height / 2 + "px";
+    circle.style.left = rect.left + rect.width / 2 + "px";
+    circle.style.top = rect.top + rect.height / 2 + "px";
 
- 
-  circle.classList.add("active");
+    circle.classList.add("active");
 
- 
-  setTimeout(() => {
-    window.location.href = "learn.html";
-  }, 700); 
-});
+    setTimeout(() => {
+      window.location.href = "learn.html";
+    }, 700);
+  });
+}
 
+const startBtn2 = document.getElementById("strbtn2");
+const circle2 = document.getElementById("transition");
+
+if (startBtn2 && circle2) {
+  startBtn2.addEventListener("click", () => {
+    const rect = startBtn2.getBoundingClientRect();
+
+    circle2.style.left = rect.left + rect.width / 2 + "px";
+    circle2.style.top = rect.top + rect.height / 2 + "px";
+
+    circle2.classList.add("active");
+
+    setTimeout(() => {
+      window.location.href = "tests1.html";
+    }, 700);
+  });
+}
+const startBtn3 = document.getElementById("strbtn3");
+const circle3 = document.getElementById("transition");
+
+if (startBtn3 && circle3) {
+  startBtn3.addEventListener("click", () => {
+    const rect = startBtn3.getBoundingClientRect();
+
+    circle3.style.left = rect.left + rect.width / 2 + "px";
+    circle3.style.top = rect.top + rect.height / 2 + "px";
+
+    circle3.classList.add("active");
+
+    setTimeout(() => {
+      window.location.href = "tests2.html";
+    }, 700);
+  });
+}
+const startBtn4 = document.getElementById("strbtn4");
+const circle4 = document.getElementById("transition");
+
+if (startBtn4 && circle2) {
+  startBtn4.addEventListener("click", () => {
+    const rect = startBtn4.getBoundingClientRect();
+
+    circle4.style.left = rect.left + rect.width / 2 + "px";
+    circle4.style.top = rect.top + rect.height / 2 + "px";
+
+    circle4.classList.add("active");
+
+    setTimeout(() => {
+      window.location.href = "tests3.html";
+    }, 700);
+  });
+}
+
+function checkTest() {
+  let score = 0;
+  const questions = document.querySelectorAll(".question");
+
+  questions.forEach(q => {
+    const correct = q.dataset.correct;
+    const inputs = q.querySelectorAll("input");
+
+    inputs.forEach((input, index) => {
+      if (input.checked && index == correct) score++;
+    });
+  });
+
+  document.getElementById("result").textContent =
+    `Результат: ${score} / ${questions.length}`;
+}
 
 
